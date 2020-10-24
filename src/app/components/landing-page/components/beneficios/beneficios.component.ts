f0c011d04522c9e60b95a3c66a5e489eb2b3444c
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-24 17:49:42 -0300

    Se crea componente de beneficios

---
+++ src/app/components/landing-page/components/beneficios/beneficios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'beneficios-component',
+  templateUrl: './beneficios.component.html',
+  styleUrls: ['./beneficios.component.css']
+})
+export class BeneficiosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
