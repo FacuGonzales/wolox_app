c7d8189b0653ce04540a6b4f88f4f6481a1deb78
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-24 12:27:11 -0300

    Se crea ruta a home y por defecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './components';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent  }, 
+  { path: 'home', component: LandingPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
