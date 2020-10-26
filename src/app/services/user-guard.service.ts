f020da50415a6547bc2dcad423a21b34ce8fe025
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-26 20:59:40 -0300

    Se arma metodo canActivate en service

--- src/app/services/user-guard.service.ts
+++ src/app/services/user-guard.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import { CanActivate, Router } from '@angular/router';
+
 @Injectable({
   providedIn: 'root'
 })
-export class UserGuardService {
+export class UserGuardService implements CanActivate{
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  canActivate(){
+    let token = localStorage.getItem('token-wolox')
+
+    if(token){
+      return true;
+    }else{
+      this.router.navigate(['/register']);
+      return false;
+    }
+  }
 }
