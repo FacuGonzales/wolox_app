7cdd3f00a409b54294abbad9f177c4ff2554ec2e
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-26 14:03:23 -0300

    Se arma metodo register

--- src/app/modules/register-page/services/register-data.service.ts
+++ src/app/modules/register-page/services/register-data.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs' 
+import { Register } from '../models/register';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterDataService {
 
-  constructor() { }
+
+  constructor(public http: HttpClient) { }
+
+  register(userRegister: Register): Observable<any>{
+    let url = 'http://private-8e8921-woloxfrontendinverview.apiary-mock.com/signup'
+
+    let params = JSON.stringify(userRegister);
+
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.http.post(url, params, {headers: headers});
+  }
+  
 }
