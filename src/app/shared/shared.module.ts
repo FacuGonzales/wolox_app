0f4281d7b91559a468119fc4dbfc0df8ca2242e9
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-25 21:45:58 -0300

    Se crea shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from './loading';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+    ],
+    declarations: [
+        LoadingComponent
+    ],
+    providers: [],
+})
+export class SharedModule { }
