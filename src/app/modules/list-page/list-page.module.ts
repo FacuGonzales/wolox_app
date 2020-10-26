d8d9feef4a15493c3eb82ad0c2c6fe26eed26f6f
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-26 16:33:05 -0300

    Se crea listPageModule

---
+++ src/app/modules/list-page/list-page.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { SharedModule } from 'src/app/shared/shared.module';
+
+import { ListPageComponent } from './page';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        SharedModule
+
+    ],
+    declarations: [
+    ListPageComponent],
+    providers: [],
+})
+export class ListPageModule { }
