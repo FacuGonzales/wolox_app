7203c6a83840cbda47ce491851fdf6bea203b88e
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-25 22:06:53 -0300

    Se borra css en desuso y se actualiza componente

--- src/app/modules/landing-page/landing-page.component.ts
+++ src/app/modules/landing-page/landing-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styles: []
 })
 export class LandingPageComponent implements OnInit {
 
