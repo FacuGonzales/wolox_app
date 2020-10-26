116ecd00eb8c6e80f4a8a15ffe028782340e06e8
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-26 20:58:45 -0300

    Se agrega can activate a ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 
+import {UserGuardService} from './services/user-guard.service';
+
 const defaultPathApp = '/';
 
 const routes: Routes = [
     {   path: '', 
         loadChildren: () => import('./modules/landing-page/landing-page.module').then(m => m.LandingPageModule),
+        
     }, 
     {   path: 'register', 
         loadChildren: () => import('./modules/register-page/register-page.module').then(m => m.RegisterPageModule),
     }, 
     {   path: 'list', 
         loadChildren: () => import('./modules/list-page/list-page.module').then(m => m.ListPageModule),
+        canActivate: [UserGuardService]
     },
     {
         path: '**', redirectTo: defaultPathApp
