b699ee7d941474188905578360d067ff270e2798
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-27 14:49:59 -0300

    Se crea metodo para obtenerTodos las tecnologias

--- src/app/modules/list-page/services/tecnologias-data.service.ts
+++ src/app/modules/list-page/services/tecnologias-data.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TecnologiasDataService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  obtenerTodos(): Observable<any>{
+    let url = 'http://private-8e8921-woloxfrontendinverview.apiary-mock.com/techs'
+
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.http.get(url, {headers: headers});
+  }
+  
 }
