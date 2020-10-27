1c81fbcbc8e11ba92f86a46c29641639984e1eaf
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-27 14:53:07 -0300

    Se arma ngDestroy para llamadas

--- src/app/modules/list-page/page/list-page.component.ts
+++ src/app/modules/list-page/page/list-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { TecnologiasDataService } from '../services/tecnologias-data.service';
 
 @Component({
-  selector: 'app-list-page',
+  selector: 'list-page',
   templateUrl: './list-page.component.html',
   styleUrls: ['./list-page.component.css']
 })
 export class ListPageComponent implements OnInit {
 
-  constructor() { }
+  subscribes: Subscription[] = [];
+
+  constructor(private tecnologiasData: TecnologiasDataService) { }
 
   ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(){
+    this.subscribes.forEach(s => s.unsubscribe());
   }
 
 }
