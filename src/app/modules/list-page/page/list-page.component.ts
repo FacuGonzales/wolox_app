a4ab26616890b84491ab7bc7dbacff4f46af979b
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-27 15:00:36 -0300

    Se arma metodo en componente para obtener listado

--- src/app/modules/list-page/page/list-page.component.ts
+++ src/app/modules/list-page/page/list-page.component.ts
@@ -8,17 +8,43 @@ import { TecnologiasDataService } from '../services/tecnologias-data.service';
   styleUrls: ['./list-page.component.css']
 })
 export class ListPageComponent implements OnInit {
+  loading: boolean = false;
 
+  tError: boolean = false;
   subscribes: Subscription[] = [];
 
   constructor(private tecnologiasData: TecnologiasDataService) { }
 
   ngOnInit(): void {
-    
+    this.obtenerLista();
   }
 
   ngOnDestroy(){
     this.subscribes.forEach(s => s.unsubscribe());
   }
 
+  obtenerLista(){
+    this.loading = true;
+
+    this.subscribes.push(this.tecnologiasData.obtenerTodos().subscribe(
+      resultado => {
+        if(resultado){
+          this.loading = false;
+          console.log(resultado);
+        }else{
+          this.loading=false
+          this.tError = true;
+          setTimeout(() => {
+              this.tError = false;
+          }, 2000);
+        }
+      },err => {
+        this.loading=false
+          this.tError = true;
+          setTimeout(() => {
+              this.tError = false;
+          }, 2000);
+      }
+    ))
+  }
 }
