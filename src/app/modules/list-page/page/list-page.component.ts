39ff30fa682e3b38aebf16b620a1c851d55be47b
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-27 21:57:11 -0300

    Se metodo para filtrar por tipo de tecnologia

--- src/app/modules/list-page/page/list-page.component.ts
+++ src/app/modules/list-page/page/list-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { Tecnologias } from '../models/tecnologia';
 import { TecnologiasDataService } from '../services/tecnologias-data.service';
 
 @Component({
@@ -8,9 +10,13 @@ import { TecnologiasDataService } from '../services/tecnologias-data.service';
   styleUrls: ['./list-page.component.css']
 })
 export class ListPageComponent implements OnInit {
+  
   loading: boolean = false;
-
   tError: boolean = false;
+
+  _tecnObtenidas: Tecnologias[] = [];
+  _listadoFiltrado: Tecnologias[] = [];
+
   subscribes: Subscription[] = [];
 
   constructor(private tecnologiasData: TecnologiasDataService) { }
@@ -28,9 +34,11 @@ export class ListPageComponent implements OnInit {
 
     this.subscribes.push(this.tecnologiasData.obtenerTodos().subscribe(
       resultado => {
+
         if(resultado){
           this.loading = false;
-          console.log(resultado);
+          this._tecnObtenidas = resultado;
+          this._listadoFiltrado = this._tecnObtenidas;
         }else{
           this.loading=false
           this.tError = true;
@@ -38,6 +46,7 @@ export class ListPageComponent implements OnInit {
               this.tError = false;
           }, 2000);
         }
+
       },err => {
         this.loading=false
           this.tError = true;
@@ -47,4 +56,17 @@ export class ListPageComponent implements OnInit {
       }
     ))
   }
+
+  filter(value){
+    let listaActualziada = this._tecnObtenidas;
+
+    if(value != ''){
+      this._listadoFiltrado = listaActualziada.filter(
+        tec => tec.type.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+      )
+    }else{
+      this._listadoFiltrado = this._tecnObtenidas;
+    }
+  }
+
 }
