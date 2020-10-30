003294b97e73773ec06d635efda9f21d2f3af55d
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-30 15:50:58 -0300

    Se modifica test de landing page

--- cypress/integration/landing-page/landing-page.spec.js
+++ cypress/integration/landing-page/landing-page.spec.js
@@ -6,42 +6,38 @@ context('LandingPage', () => {
         cy.visit('http://localhost:4200');
     })
 
-    it('Debo poder scrollear dentro de la página', () => {
-        cy.wait(2500);
-        cy.scrollTo('center');
-        cy.wait(2000);
-        cy.scrollTo('bottom');
-        cy.wait(2000);
-        cy.scrollTo('center');
-        cy.wait(2000);
-        cy.scrollTo('top');
-        cy.wait(2000);
-        cy.scrollTo('center');
+    it('Debo poder ir a la Seccion de Inicio', () => {
+        cy.wait(3500);
+        cy.get('#inicioId').click()
+        cy.wait(3500);
     })
 
     it('Debo poder hacer click en "Siguenos"', () => {
-        cy.wait(2500);
+        cy.wait(3500);
         cy.get('#buttonSiguenos').click();
+        cy.wait(3500);
+        cy.scrollTo('top');
+        cy.wait(3500);
+    })
+
+    it('Debo poder ir a la Seccion de Beneficios', () => {
+        cy.wait(3500);
+        cy.get('#beneficiosId').click();
+        cy.wait(3500);
+        cy.scrollTo('top');
+        cy.wait(3500);
+    })
+
+    it('Debo poder scrollear al footer', () => {
+        cy.wait(3500);
+        cy.scrollTo('bottom');
+        cy.wait(3500);
     })
 
     it('Debo poder hacer click en "Conocer más"', () => {
-        cy.wait(2500);
+        cy.wait(3500);
         cy.get('#buttonConocerMas').click();
-    })
-
-    it('Debo poder ir a "Inicio" y luego a "Beneficios"', () => {
-        cy.wait(2500);
-        cy.scrollTo('top');
-        cy.wait(2500);
-        cy.get('#inicioId').click();
-        cy.wait(5000);
-        cy.get('#beneficiosId').click();
-        cy.wait(3000);
-    })
-
-    it('Debo poder volver a "Home"', () => {
-        cy.wait(3000);
-        cy.get('#homeId').click();
+        cy.wait(3500);
     })
 
     it('Debo poder ir a "Register"', () => {
