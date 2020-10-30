f0ff6c92a09690477bd28caaea274dfb449bd43c
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-29 23:44:01 -0300

    Se crean routings

--- src/app/modules/landing-page/landing-page-routing.module.ts
+++ src/app/modules/landing-page/landing-page-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BeneficiosComponent, InicioComponent } from './components';
 import { LandingPageComponent } from './landing-page.component';
 
 
 const routes: Routes = [
   {
     path: '', component: LandingPageComponent
+  },
+  {
+    path: 'inicio', component: InicioComponent
+  },
+  {
+    path: 'beneficios', component: BeneficiosComponent
   }
 ];
 
