b198f94d4fc63244970491303824d7f62313f7f3
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-30 15:35:47 -0300

    Se arman rutas hijas

--- src/app/modules/landing-page/landing-page-routing.module.ts
+++ src/app/modules/landing-page/landing-page-routing.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BeneficiosComponent, InicioComponent } from './components';
+import { BeneficiosComponent, HomeComponent, InicioComponent } from './components';
 import { LandingPageComponent } from './landing-page.component';
 
 
 const routes: Routes = [
   {
-    path: '', component: LandingPageComponent
+    path: '', component: LandingPageComponent,
+    children: [
+      {
+        path: 'home', component: LandingPageComponent
+      },
+      {
+        path: 'inicio', component: InicioComponent
+      },
+      {
+        path: 'beneficios', component: BeneficiosComponent
+      }
+    ]
   },
-  {
-    path: 'inicio', component: InicioComponent
-  },
-  {
-    path: 'beneficios', component: BeneficiosComponent
-  }
+
 ];
 
 @NgModule({
