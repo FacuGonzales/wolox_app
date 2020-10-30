37bb9e6af2dfeaa11990df0413e4581c1697eae1
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-30 10:21:44 -0300

    Se crea test de registerPageComponent

--- cypress/integration/register-page/register-page.spec.js
+++ cypress/integration/register-page/register-page.spec.js
@@ -5,16 +5,28 @@ context('LandingPage', () => {
         cy.visit('http://localhost:4200/register');
     })
 
-    it('Debo poder completar el campo nombre', () => {
+    it('Debo poder rellenar el formulario y registrarme', () => {
         cy.wait(2500);
         cy.get('#nombreId').type('Prueba')
         cy.wait(2000);
-    })
-
-    it('Debo poder completar el campo apellido', () => {
-        cy.wait(2500);
         cy.get('#apellidoId').type('Apellido Prueba')
         cy.wait(2000);
+        cy.get('#paisId').select('Argentina')
+        cy.wait(2000);
+        cy.get('#provinciaId').select('Córdoba')
+        cy.wait(2000);
+        cy.get('#emailId').type('prueba@gmail.com')
+        cy.wait(2000);
+        cy.get('#telefonoId').type('351000012')
+        cy.wait(2000);
+        cy.get('#passwordId').type('1234Prueba')
+        cy.wait(2000);
+        cy.get('#rpPasswordId').type('1234Prueba')
+        cy.wait(2000);
+        cy.get('#checkId').click();
+        cy.wait(3000);
+        cy.get('#buttonEnviar').click();
     })
 
+    
 })
