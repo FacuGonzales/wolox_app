dee5b8c8ab6175f386bc1129c4dc429d23e3a6fd
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-29 23:40:50 -0300

    Se obtiene url de inicio

--- src/app/modules/landing-page/components/inicio/inicio.component.ts
+++ src/app/modules/landing-page/components/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'inicio-component',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  url: string = ''
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.url = this.router.url;
   }
 
 }
