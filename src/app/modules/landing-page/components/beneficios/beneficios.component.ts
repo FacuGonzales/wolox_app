16b4be790e92ea4ed19117fa4e65e114413aa751
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-10-29 23:41:31 -0300

    Se obtiene url

--- src/app/modules/landing-page/components/beneficios/beneficios.component.ts
+++ src/app/modules/landing-page/components/beneficios/beneficios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'beneficios-component',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeneficiosComponent implements OnInit {
 
-  constructor() { }
+  url: string = ''
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.url = this.router.url;
   }
+
 
 }
