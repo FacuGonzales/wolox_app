43879c17b4ea4466484aee9e512622c04078dc7b
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-11-01 17:54:43 -0300

    Se crea array de idiomas, se crean metodos para setear idoma por defecto, se crea metodo para cambiar idioma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
+  lang: string[] = [];
 
+  constructor(private translate: TranslateService){
+    this.translate.setDefaultLang('es');
+    this.translate.use('es');
+    this.translate.addLangs(['es', 'en']);
+    this.lang = this.translate.getLangs();
+  }
+
+  changeLang(lang: string){
+    this.translate.use(lang);
+  }
 }
